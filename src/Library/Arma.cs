7eb84bfc8dcf054d92d8e056cf840c931f34e785
Joaquin Rodriguez <torito@MacBook-Air-de-Joaquin.local>
2026-09-14 19:47:31 -0300

    Agrego clases Arma y Ropaje

---
+++ src/Library/Arma.cs
@@ -0,0 +1,11 @@
+namespace Library;
+
+public abstract class Arma
+{
+    private int valorAtaque;
+
+    public Arma(int valorAtaque)
+    {
+        this.valorAtaque = valorAtaque;
+    }
+}
---
+++ src/Library/Ropaje.cs
@@ -0,0 +1,11 @@
+namespace Library;
+
+public class Ropaje : Item
+{
+    private int valorDefensa;
+
+    public Ropaje(int valorDefensa)
+    {
+        this.valorDefensa = valorDefensa;
+    }
+}
