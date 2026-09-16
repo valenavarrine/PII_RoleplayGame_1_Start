15ce7cb52649f79fe2c29acb74ffe9f23ac4254a
Matias Ubbriaco <mhubb923@gmail.com>
2026-09-16 20:25:47 -0300

    Arreglo menor

--- src/Library/Arma.cs
+++ src/Library/Arma.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
 
-namespace  Ucu.Poo.RolePlayGame;
+namespace  Ucu.Poo.RolePlayGame{
 
 public class Arma : Item
 {
     public Arma(int valorAtaque) : base(valorAtaque,0)
     {
     }
+}
 }
